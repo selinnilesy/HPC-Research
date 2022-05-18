76f25432bd8e9b951b314eb4b4560eae6b367dac
Selin <selin.yildirim@metu.edu.tr>
2022-05-18 15:33:14 +0300

    change computation

--- SSSconflictFree.cpp
+++ SSSconflictFree.cpp
@@ -7,54 +7,48 @@
 
 using namespace std;
 
-vector<double> val;
-vector<int> row,col;
-int *rowPtr, *colPtr;
-double *valPtr;
+vector<double> lowerRes,upperRes, totalRes,diag,  sum;
 int nnz;
 
-extern "C" {
-    extern void amux_(int *n, double* x, double *y, double *a, int *ja, int *ia);
-}
-
-int readCSRStorage(int z, double ratio, bool lower) {
-    cout <<  " start reading Upper CSR Storage..." << endl;
+int readResult(int z, double ratio) {
+    cout <<  " start reading multip result lower/upper..." << endl;
 
     double doubleVal;
-    int intVal;
-    string fileName, rowfile, colfile, valfile;
-    int counter=0;
+    string fileName;
     // middle read not yet IMPLEMENTED !!!
     // TO DO
-    if(!lower) fileName = "/home/selin/Split-Data/" + matrix_names[z]  + "/inner-outer-equal/inner/CSR-Data/upper";
-    else fileName = "/home/selin/Split-Data/" + matrix_names[z] + "/inner-outer-equal/inner/CSR-Data" ;
-
-    rowfile = fileName + "/" + to_string(ratio) + "-row.txt";
-    colfile = fileName + "/" + to_string(ratio) + "-col.txt";
-    valfile = fileName + "/" + to_string(ratio) + "-val.txt";
-
-    std::fstream myfile(rowfile, std::ios_base::in);
-    while (myfile >> intVal) {
-        row.push_back(intVal);
-    }
-    myfile.close();
-    cout << rowfile << " has been read with size: " << row.size() << endl;
-
-    myfile.open(colfile, std::ios_base::in);
-    while (myfile >> intVal) {
-        col.push_back(intVal);
-    }
-    myfile.close();
-
-    nnz=col.size();
-    cout << colfile << " has been read with size: " << col.size() << endl;
-
-    myfile.open(valfile, std::ios_base::in);
+    fileName = "/home/selin/Split-Data/" + matrix_names[z]  + "/inner-outer-equal/inner/CSR-Data/upper/" + to_string(ratio)+ "-result.txt";
+    std::fstream myfile(fileName, std::ios_base::in);
     while (myfile >> doubleVal) {
-        val.push_back(doubleVal);
+        upperRes.push_back(doubleVal);
     }
     myfile.close();
-    cout << valfile << " has been read with size: " << val.size() << endl;
+    cout << fileName << " has been read with size: " << upperRes.size() << endl;
+
+    fileName = "/home/selin/Split-Data/" + matrix_names[z]  + "/inner-outer-equal/inner/CSR-Data/" + to_string(ratio)+ "-result.txt";
+    myfile.open(fileName, std::ios_base::in);
+    while (myfile >> doubleVal) {
+        lowerRes.push_back(doubleVal);
+    }
+    myfile.close();
+    nnz=lowerRes.size();
+    cout << fileName << " has been read with size: " << lowerRes.size() << endl;
+
+    fileName = "/home/selin/Outputs/" + matrix_names[z]  + "/inner-" + to_string(ratio)+ ".txt";
+    myfile.open(fileName, std::ios_base::in);
+    while (myfile >> doubleVal) {
+        totalRes.push_back(doubleVal);
+    }
+    myfile.close();
+    cout << fileName << " has been read with size: " << totalRes.size() << endl;
+
+    fileName = "/home/selin/SSS-Data/" + matrix_names[z]  + "/diag.txt";
+    myfile.open(fileName, std::ios_base::in);
+    while (myfile >> doubleVal) {
+        diag.push_back(doubleVal);
+    }
+    myfile.close();
+    cout << fileName << " has been read with size: " << diag.size() << endl;
     return 0;
 }
 
@@ -64,55 +58,24 @@ int main(int argc, char **argv)
     int n = matrixSize[atoi(argv[1])];
     int inputType = atoi(argv[1]);
     double inputRatio = atof(argv[2]);
-
-    bool lower = atoi(argv[3]);
     cout << "input ratio: " << inputRatio << endl;
-    cout << "lower bool: " << lower << endl;
 
-    readCSRStorage( inputType, inputRatio, lower);
-
-    rowPtr = new int[matrixSize[inputType] + 1];
-    colPtr = new int[nnz];
-    valPtr = new double[nnz];
-
-
-    for(int i=0; i<row.size(); i++) rowPtr[i] = row[i];
-    for(int i=0; i<col.size(); i++) colPtr[i] = col[i];
-    for(int i=0; i<val.size(); i++) valPtr[i] = val[i];
-
-    cout << "test: " << rowPtr[100] << endl;
-    cout << "test: " << colPtr[100] << endl;
-    cout << "test: " << valPtr[100] << endl;
-    int nrow= matrixSize[inputType];
-
-    double* X = new double[nrow];
-    for(int i=0; i<nrow; i++) X[i] = 1.0;
-
-    double* Y = new double[nrow];
-    for(int i=0; i<nrow; i++) Y[i] = 0.0;
-
-    cout << "starts computing amux..." << endl;
-    amux_(&nrow, X, Y, valPtr, colPtr, rowPtr);
-    std::cout  <<  " finished computing amux_... " << endl;
+    readResult(inputType, inputRatio);
+    cout << "test: " << lowerRes[0] << endl;
+    cout << "test: " << upperRes[0] << endl;
+    cout << "test: " << totalRes[0] << endl;
+    cout << "test: " << diag[0] << endl;
 
 
-    ofstream myfile;
-    string output;
-    if(lower) output = "/home/selin/Split-Data/" + matrix_names[inputType]  + "/inner-outer-equal/inner/CSR-Data/" + to_string(inputRatio) + "-result.txt";
-    else if(!lower) output = "/home/selin/Split-Data/" + matrix_names[inputType]  + "/inner-outer-equal/inner/CSR-Data/upper/" + to_string(inputRatio) + "-result.txt";
-    myfile.open(output, ios::out | ios::trunc);
-
-    cout << "Writing result: " << endl;
-    for(int i=0; i<nrow; i++) {
-        myfile << Y[i] << " " ;
+    for(int i=0; i<lowerRes.size(); i++){
+        sum.push_back(lowerRes[i] + upperRes[i] + diag[i]);
     }
-    myfile.close();
-    cout << "Output completed." << endl;
-
-    delete [] X;
-    delete [] Y;
-    delete [] rowPtr;
-    delete [] colPtr;
-    delete [] valPtr;
+    for(int i=0; i<sum.size(); i++){
+        if(sum[i] != totalRes[i]) {
+            cout << "not equal index: " << i << " you found: " <<sum[i] << " banded computed: " << totalRes[i];
+            return -1;
+        }
+    }
+    return 0;
 
 }
