4e1c3d6ea78f7dd067524a6bdec1635a522e0e8c
Selin <selin.yildirim@metu.edu.tr>
2022-05-23 01:31:18 +0300

    fix compile errors

--- SSSconflictFree.cpp
+++ SSSconflictFree.cpp
@@ -10,7 +10,7 @@ using namespace std;
 vector<double> lowerRes,upperRes, totalRes,diag,  sum;
 int nnz;
 
-int readResult(int z, double ratio, double mratio) {
+int readResult(int z, double ratio, double mratio, int innerB) {
     cout <<  " start reading multip result lower/upper..." << endl;
 
     double doubleVal;
@@ -34,14 +34,6 @@ int readResult(int z, double ratio, double mratio) {
     nnz=lowerRes.size();
     cout << fileName << " has been read with size: " << lowerRes.size() << endl;
 
-    fileName = "/home/selin/Outputs/" + matrix_names[z]  + "/middle-" + to_string(ratio)+ "-" + to_string(mratio) + ".txt";
-    myfile.open(fileName, std::ios_base::in);
-    while (myfile >> doubleVal) {
-        totalRes.push_back(doubleVal);
-    }
-    myfile.close();
-    cout << fileName << " has been read with size: " << totalRes.size() << endl;
-
     fileName = "/home/selin/SSS-Data/" + matrix_names[z]  + "/diag.txt";
     myfile.open(fileName, std::ios_base::in);
     while (myfile >> doubleVal) {
@@ -49,6 +41,19 @@ int readResult(int z, double ratio, double mratio) {
     }
     myfile.close();
     cout << fileName << " has been read with size: " << diag.size() << endl;
+
+    fileName = "/home/selin/Outputs/" + matrix_names[z]  + "/middle-" + to_string(ratio)+ "-" + to_string(mratio) + ".txt";
+    myfile.open(fileName, std::ios_base::in);
+    for (int i=0; i<innerB+1; i++)
+    {
+        totalRes.push_back(diag[i]);
+    }
+    while (myfile >> doubleVal) {
+        totalRes.push_back(doubleVal);
+    }
+    myfile.close();
+    cout << fileName << " has been read with size: " << totalRes.size() << endl;
+
     return 0;
 }
 
@@ -58,11 +63,13 @@ int main(int argc, char **argv)
     int n = matrixSize[atoi(argv[1])];
     int inputType = atoi(argv[1]);
     double inputRatio = atof(argv[2]);
-    double mr = atof(argv[3]);
+    double middleRatio = atof(argv[3]);
     cout << "input ratio: " << inputRatio << endl;
-    cout << "middle ratio: " << mr << endl;
+    cout << "middle ratio: " << middleRatio << endl;
 
-    readResult(inputType, inputRatio, mr);
+    int innerBandwith = (int) (nnz_n_Ratios[inputType]*bandwithProportions[inputType] * inputRatio);
+    int middleBandwith = (int) ((bandwithSize[inputType] - innerBandwith)*middleRatio);
+    readResult(inputType, inputRatio, middleRatio, innerBandwith);
     cout << "test: " << lowerRes[0] << endl;
     cout << "test: " << upperRes[0] << endl;
     cout << "test: " << totalRes[0] << endl;
