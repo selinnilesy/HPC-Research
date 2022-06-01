36cdf26d0ba14e9eb022376a841dcf1025c4e5a9
Selin <selin.yildirim@metu.edu.tr>
2022-06-01 21:49:09 +0300

    working dgbmv

--- SSSconflictFree.cpp
+++ SSSconflictFree.cpp
@@ -1,139 +1,228 @@
-//
-// Created by Selin Yıldırım on 7.01.2022.
-//
-
+#include <cblas.h>
+#include <stdio.h>
 #include <iostream>
-#include  "header.h"
-//#include <mpi.h>
-//#include "rcmtest.cpp"
-//#include "geeks.cpp"
+#include <cmath>
 
 using namespace std;
-#define MATRIX_COUNT 6
 
-int readSSSFormat(int z, bool banded) {
-    fs::path matrixFolder;
-    if(!banded) matrixFolder = "/home/selin/SSS-Data/" + matrix_names[z] + "/unbanded" ;
-    else matrixFolder = "/home/selin/SSS-Data/" + matrix_names[z];
-    for(auto const& dir_entry: fs::directory_iterator{matrixFolder}){
-        std::fstream myfile(dir_entry.path(), std::ios_base::in);
-        if(dir_entry.path().stem() == "rowptr") {
-            int tempValInt;
-            vector<int> tempVecInt;
-            while (myfile >> tempValInt) {
-                tempVecInt.push_back(tempValInt);
+
+int main()
+{
+    int i=0;
+    int size= 10;
+    double** matrix = new double*[size];
+    double** matrix2 = new double*[5];
+
+    // generate symmetric banded matrix
+
+    for(int i=0; i<10; i++){
+        matrix[i] = new double[10];
+        for(int j=0; j<10; j++) {
+            if(j < i-2 || j> i+2 ) matrix[i][j] = 0.0;
+            else if(i==j){
+                matrix[i][j] = 1.0;
+                if(i > 0 && j < 9){
+                    matrix[i][j-1] = 5.0;
+                    matrix[i][j+1] = 5.0;
+                }
+
+                if(i > 1 && j < 8){
+                    matrix[i][j-2] = 2.0;
+                    matrix[i][j+2] = 2.0;
+                }
+
             }
-            rowptrPtrs.push_back(new int[tempVecInt.size()]);
-            int *temp = rowptrPtrs[0];
-            for(int i=0; i<tempVecInt.size(); i++) temp[i]=tempVecInt[i];
-            rowptrSize.push_back(tempVecInt.size());
-            cout << dir_entry.path() << " has been read with size: " <<tempVecInt.size() << endl;
-            myfile.close();
         }
-        else if(dir_entry.path().stem() == "colind") {
-            int tempValInt;
-            vector<int> tempVecInt;
-            while (myfile >> tempValInt) {
-                tempVecInt.push_back(tempValInt);
+    }
+    matrix[9][8] = 5.0;
+    matrix[9][7] = 2.0;
+    matrix[0][1] = 5.0;
+    matrix[0][2] = 2.0;
+    matrix[8][6] = 2.0;
+    matrix[1][3] = 2.0;
+
+
+    /*
+    for(int i=0; i<5; i++){
+        matrix[i] = new float[5];
+        for(int j=0; j<5; j++) {
+            if(j < i-1 || j> i+1 ) matrix[i][j] = 0.0;
+            else if(i==j){
+                matrix[i][j] = 1.0;
+                if(i > 0 && j < 4){
+                    matrix[i][j-1] = 1.0;
+                    matrix[i][j+1] = 1.0;
+                }
             }
-            colindPtrs.push_back(new int[tempVecInt.size()]);
-            int *temp = colindPtrs[0];
-            for(int i=0; i<tempVecInt.size(); i++) temp[i]=tempVecInt[i];
-            colindSize.push_back(tempVecInt.size());
-            cout << dir_entry.path() << " has been read with size: " <<  tempVecInt.size() << endl;
-            myfile.close();
-        }
-        else if(dir_entry.path().stem() == "diag"){
-            double tempVal;
-            vector<double> tempVec;
-            while (myfile >> tempVal) {
-                tempVec.push_back(tempVal);
-            }
-            dvaluesPtrs.push_back(new double[tempVec.size()]);
-            double *temp = dvaluesPtrs[0];
-            for(int i=0; i<tempVec.size(); i++) temp[i]=tempVec[i];
-            dvaluesSize.push_back(tempVec.size());
-            cout << dir_entry.path() << " has been read with size: " <<  tempVec.size() << endl;
-            myfile.close();
-        }
-        else if(dir_entry.path().stem() == "vals"){
-            double tempVal;
-            vector<double> tempVec;
-            while (myfile >> tempVal) {
-                tempVec.push_back(tempVal);
-            }
-            valuesPtrs.push_back(new double[tempVec.size()]);
-            double *temp = valuesPtrs[0];
-            for(int i=0; i<tempVec.size(); i++) temp[i]=tempVec[i];
-            valuesSize.push_back(tempVec.size());
-            cout << dir_entry.path() << " has been read with size: " <<  tempVec.size() << endl;
-            myfile.close();
-        }
-        else cout << "unexpected file name: " << dir_entry.path() << endl;
-    }
-    return 0;
-}
-/*
-    A RNxN : matrix in SSS format
-    x RN : input vector
-    y RN : output vector
-
- */
-
-int main(int argc, char **argv){
-    int n, rowLimit;
-    cout << "i call read CSR Format. " << endl;
-    //init();
-    if(!argv[1]){
-        cout << "please provide input matrix index (int): boneS10, Emilia_923, ldoor, af_5_k101, Serena, audikw_1" << endl;
-        return -1;
-    }
-    if(!argv[2]){
-        cout << "please provide bool for banded" << endl;
-        return -1;
-    }
-    bool banded = atoi(argv[2]);
-    readSSSFormat(atoi(argv[1]) , banded);
-
-    n = matrixSize[atoi(argv[1])];
-    int inputType = atoi(argv[1]);
-
-
-    double *matrixOffDiagonal = valuesPtrs[0];
-    double *matrixDiagonal = dvaluesPtrs[0];
-    int *matrixColind = colindPtrs[0];
-    int *matrixRowptr= rowptrPtrs[0];
-    double *x = new double[n];
-    double *y = new double[n];
-    for(int i=0; i<n; i++) x[i] = 1.0;
-    for(int i=0; i<n; i++) y[i] = 0.0;
-    //memset(y, 0, (size_t) (n*sizeof(double)));
-
-    int colInd;
-    cout << "start computing sequential ssbmv..." << endl;
-    clock_t t = clock();
-    for(int i=0; i<n; i++){
-        y[i] = matrixDiagonal[i] * x[i];
-        for(int j=matrixRowptr[i]-1; j<matrixRowptr[i+1]-1; j++){
-            colInd = matrixColind[j] - 1;
-            y[i] += matrixOffDiagonal[j] * x[colInd];
-            y[colInd] += matrixOffDiagonal[j]*x[i];
         }
     }
-    t = clock() - t;
-    cout << "finished computing sequential ssbmv." << endl;
-    printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+    matrix[0][1] = matrix[4][3]  = 1.0;
+    */
 
 
-    ofstream myfile1;
-    if(!banded) myfile1.open ("/home/selin/Seq-Results/" + matrix_names[inputType] + "/unbanded/result.txt", ios::out | ios::trunc);
-    else myfile1.open ("/home/selin/Seq-Results/" + matrix_names[inputType] + "/banded/result.txt", ios::out | ios::trunc);
-
-    cout << "Writing to output. " << endl;
-    for (int i=0; i<n; i++) {
-        myfile1 << y[i] << '\t';
+    for(int i=0; i<5; i++){
+        matrix2[i] = new double[5];
+        for(int j=0; j<5; j++) {
+            matrix2[i][j] = 1.0;
+        }
     }
-    myfile1.close();
+    matrix2[0][3] = matrix2[0][4] = matrix2[1][4]  = matrix2[3][0] = matrix2[4][0] = matrix2[4][1] =0.0;
 
-    return 0;
+    cout << "Matrix" << endl;
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++) {
+            cout << matrix[i][j] << " " ;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    /*
+    cout << "Matrix 2" << endl;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++) {
+            cout << matrix2[i][j] << " " ;
+        }
+        cout << endl;
+    }
+     */
+
+    double* X = new double[size];
+    for(int i=0; i<size; i++) X[i] = 1.0;
+    double* Y = new double[size];
+    for(int i=0; i<size; i++) Y[i] = 0.0;
+    double alpha = 1;
+    double beta = 0;
+    //int k = 2;
+    int kl = 2;
+    int ku=2;
+    //int lda = k+1;
+    int lda=kl+ku+1;
+    int incx = 1;
+    int incy = 1;
+    double* A = new double[lda*size];
+    double* B = new double[lda*size-1];
+
+    int m;
+    // for LOWER only
+
+
+    /*
+    for(int j=1; j<=size; j++) {
+        m = 1 - j;
+        for(int i=j ; i<=min(size, j+k); i++){
+            int ind_x = i-1;
+            int ind_y = j-1;
+            A[(m+i-1)*size + ind_y] = matrix[ind_x][ind_y];
+        }
+    }
+
+
+    // for upper storage of A with size=10
+
+
+    A[0]=0;
+    A[1]=0;
+    A[size]=0;
+    A[size -1 + size]=1;
+    A[2*size + size - 1]=1;
+    A[2*size + size - 2]=1;
+     */
+    // for upper storage of A with size=5 k=1
+    /*
+    A[0]=0;
+    A[2*size -1 ]=1;
+     */
+    // for lower storage of A with size=10 k=2
+    /*
+    A[0]=1;
+    A[1]=1;
+    A[size]=1;
+    A[3*size - 1 ]=0;
+    A[3*size - 2 ]=0;
+    A[2*size - 1 ]=0;
+    */
+
+
+    for(int i=0; i<size; i++) {
+        for(int j=0 ; j<lda; j++){
+            A[(lda)*i + j]= 1;
+        }
+    }
+
+
+   for(int i=0; i<size; i++) {
+      A[i*lda + 2]= matrix[i][i];
+      if(i >=1) A[(i)*lda + 1]= matrix[i][i-1];
+      if(i >=2) A[(i)*lda +0]= matrix[i][i-2];
+   }
+   A[0] =0;
+   A[1]=0;
+   A[lda]=0;
+    for(int i=0; i<size; i++) {
+        A[(i)*lda + (lda-2)]= matrix[i][i+1];
+        A[(i)*lda +(lda-1)]= matrix[i][i+2];
+    }
+
+    A[lda*(size-1) + lda-1]=0;
+    A[lda*(size-1) + lda-1]=0;
+    A[(lda)*(size-2) + lda-1]=0;
+
+
+    /*
+    for(int i=0; i<size; i++) {
+        A[i*lda + (lda-1)]= matrix[i][i];
+        if(i >=1) A[(i)*lda + (lda-2)]= matrix[i][i-1];
+        if(i >=2) A[(i)*lda +(lda-3)]= matrix[i][i-2];
+    }
+    for(int i=0; i<size-1; i++) {
+        if(i >=1) B[(i)*lda + 0]= matrix[i][i-2];
+    }
+    B[(1)*lda + 0] = 2;
+     */
+
+
+    // col major - upper  10 x 3
+
+    //A[0]=0;
+    //A[1]=0;
+    //A[lda]=0;
+
+    // col major - lower  10 x 3
+
+    //A[size*lda-1]=0;
+    //A[size*lda-2]=0;
+    //A[(size-1)*lda-1]=0;
+
+
+    cout << "Formed A: " << endl;
+
+    for(int i=0; i<size; i++) {
+        for(int j=0 ; j<lda; j++){
+            cout << A[(lda)*i + j] << " " ;
+        }
+        cout <<  endl;
+    }
+    /*
+     * cout << "Formed B: " << endl;
+
+    for(int i=0; i<size-1; i++) {
+        for(int j=0 ; j<lda; j++){
+            cout << B[(lda)*i + j] << " " ;
+        }
+        cout <<  endl;
+    }
+     */
+    cout << "Call cblas_dgbmv. " << endl ;
+    // CblasRowMajor = 101
+    // CblasLower = 122
+    // CBLAS_TRANSPOSE
+    //const CBLAS_UPLO Uplo = CblasLower;
+    // column major : upper verince kernel lower'a giriyor. lower verince upper'a.
+    // column major : CblasLower -> ( ! kernel'de upper) 10x10 1 bandwith icin calisiyor.
+    cblas_dgbmv(CblasRowMajor, CblasNoTrans , size, size, kl, ku, alpha, A, lda, X, incx, beta, Y, incy);
+
+    cout << "Output: " << endl;
+    for(int j=0; j<size; j++) {
+        cout << Y[j] << endl;
+    }
 }
