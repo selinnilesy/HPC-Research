4626544a3c21431eaba72a6dc7fe74d8d1ddf8bb
Selin <selin.yildirim@metu.edu.tr>
2022-06-05 00:50:33 +0300

    compare serial banded with dsbmv

--- SSSconflictFree.cpp
+++ SSSconflictFree.cpp
@@ -6,11 +6,11 @@
 #include "header.h"
 
 using namespace std;
-vector<double> dgbmv_res;
+vector<double> serialBanded_res;
 vector<double> dsbmv_res;
 
-int readSSBMVResult(int z, double ratio) {
-    cout <<  " start readSSBMVResult..." << endl;
+int readDSBMVResult(int z, double ratio) {
+    cout <<  " start readDSBMVResult..." << endl;
     double doubleVal;
     int intVal;
     const fs::path matrixFolder{"/home/selin/Outputs/" + matrix_names[z]};
@@ -23,28 +23,30 @@ int readSSBMVResult(int z, double ratio) {
             while (myfile >> doubleVal) {
                 dsbmv_res.push_back(doubleVal);
             }
+           //cout << dsbmv_res[dsbmv_res.size()-3] << " " << dsbmv_res[dsbmv_res.size()-2] << " "<< dsbmv_res[dsbmv_res.size()-1];
+
             myfile.close();
             cout << dir_entry.path().string() << " has been read with size: " << dsbmv_res.size() << endl;
         }
     }
     return 0;
 }
-int readDGBMVResult(int z, double ratio) {
-    cout <<  " start readDGBMVResult..." << endl;
+int readSerialBandedResult(int z, double ratio) {
+    cout <<  " start readSerialBandedResult..." << endl;
     double doubleVal;
     int intVal;
-    const fs::path matrixFolder{"/home/selin/Outputs/" + matrix_names[z]};
+    const fs::path matrixFolder{"/home/selin/Seq-Results/" + matrix_names[z] +"/banded"};
     for(auto const& dir_entry: fs::directory_iterator{matrixFolder}) {
-        if ( dir_entry.path().stem() == ("dgbmv-inner-" + to_string(ratio))) {
+        if ( dir_entry.path().stem() == "result") {
 
             fstream myfile;
             myfile.open(dir_entry.path().string(), std::ios_base::in);
 
             while (myfile >> doubleVal) {
-                dgbmv_res.push_back(doubleVal);
+                serialBanded_res.push_back(doubleVal);
             }
             myfile.close();
-            cout << dir_entry.path().string() << " has been read with size: " << dgbmv_res.size() << endl;
+            cout << dir_entry.path().string() << " has been read with size: " << serialBanded_res.size() << endl;
         }
     }
     return 0;
@@ -59,19 +61,21 @@ int main(int argc, char **argv)
 
     cout << "input ratio: " << inputRatio << endl;
     // inner read = 1 , middle read = 0 !!!
-    readSSBMVResult(inputType, inputRatio);
-    readDGBMVResult(inputType, inputRatio);
+    readDSBMVResult(inputType, inputRatio);
+    readSerialBandedResult(inputType, inputRatio);
 
 
-    if(dgbmv_res.size() != dsbmv_res.size()){
+    if(serialBanded_res.size() != dsbmv_res.size()){
         cout << "not equal size !!! " << endl;
         return -1;
     }
 
 
-    for(int i=0; i<dgbmv_res.size(); i++){
-        cout << "not equal index: " << i << ". vals: " << dgbmv_res[i] << "," << dsbmv_res[i] << endl;
-        return -1;
+    for(int i=0; i<dsbmv_res.size(); i++){
+        if(serialBanded_res[i] != dsbmv_res[i]){
+            cout << "not equal index: " << i << ". vals: " << serialBanded_res[i] << "," << dsbmv_res[i] << endl;
+            return -1;
+        }
     }
     cout << "all equal. " << endl;
     return 0;
