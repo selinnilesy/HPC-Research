f3f527c07749b96553181fd4f7480a0ad9b98a2f
Selin <selin.yildirim@metu.edu.tr>
2022-06-05 13:58:41 +0300

    csr validated. its ok whhen compared with previous written band storage

--- SSSconflictFree.cpp
+++ SSSconflictFree.cpp
@@ -153,8 +153,8 @@ int main(int argc, char **argv)
     cout << endl;
     cout << "checking DSBMV: " << endl;
     for(int i=0; i<dsbmvRes.size(); i++){
-        if(abs( (-lowerRes[i] + upperRes[i] + diag[i] )- dsbmvRes[i]) > 0.1) {
-            cout << "not equal - index: " << i << " correct result: " <<  (-lowerRes[i] + upperRes[i] + diag[i] ) << " dsbmv computed: " << dsbmvRes[i] << endl;
+        if(abs( (lowerRes[i] + upperRes[i] + diag[i] )- dsbmvRes[i]) > 0.1) {
+            cout << "not equal - index: " << i << " correct result: " <<  (lowerRes[i] + upperRes[i] + diag[i] ) << " dsbmv computed: " << dsbmvRes[i] << endl;
             break;
         }
     }
